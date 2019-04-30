c5b00ad73edb82c5356a13e371c43a83f3a645b3
yohei3rdoct <yohei3rdoct@gmail.com>
2019-05-01 00:40:31 +0900

    ナビのレイアウト

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.layout')
 
-@section('title','第二引数で表記できる')
+@section('title','TOP')
 
 @section('contents')
 
 <img src="http://placehold.jp/30/dddddd/0000ff/1024x400.jpg?text=店舗のカウンターとかの目立つかっこいい写真">
+
+<!-- <img src="https://unsplash.it/1024/400?random"> -->
 
 @endsection
