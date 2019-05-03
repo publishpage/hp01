c8e656ee01d82552725f2d7370b52af5cc847e92
yohei3rdoct <yohei3rdoct@gmail.com>
2019-05-04 06:46:13 +0900

    管理者用ログイン情報　Todo user_idでのログインに変更する

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
+
 {
     /**
      * Run the database seeds.
@@ -11,6 +14,13 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        db::table('users')->insert([
+        	'user_id' => 'admin',
+        	'name' => '管理者',
+        	'email' => 'admin@mail.com',
+        	'password' => bcrypt('admin0000'),
+        	'created_at' => Carbon::now(),
+        	'updated_at' => Carbon::now(),
+        ]);
     }
 }
