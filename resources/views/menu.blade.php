162d329fc2145bf2139ad249220b33cdc454882f
yohei3rdoct <yohei3rdoct@gmail.com>
2019-05-04 05:47:23 +0900

    Menu追加

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.layout')
+
+@section('title','TOP')
+
+@section('contents')
+
+<!-- メニュー1 -->
+<div class="container">
+  <img class="img-fluid mx-auto d-block" src="https://unsplash.it/640/300?random">
+</div>
+<!-- レスポンシブに対応した画像表示 管理画面での画像張替え機能 -->
+
+<!-- メニュー2 -->
+<div class="container">
+  <img class="img-fluid mx-auto d-block" src="https://unsplash.it/640/301?random">
+</div>
+
+<!-- メニュー3 -->
+<div class="container">
+  <img class="img-fluid mx-auto d-block" src="https://unsplash.it/640/302?random">
+</div>
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/menu', function () {
+    return view('menu');
+});
